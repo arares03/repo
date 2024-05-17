7c544710e385662451ea50301a88814f4cc06bec
randreican <rares.andreican@8x8.com>
2024-05-17 21:10:17 +0300

    initial commit

---
+++ src/test/java/com/example/siemensinternship/SiemensInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.siemensinternship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiemensInternshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
